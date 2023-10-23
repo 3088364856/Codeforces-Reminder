4365cae2a5852f0ff87b082594bd715cc9fe187a
fri59h2 <103104870+3088364856@users.noreply.github.com>
2023-10-23 13:31:09 +0800

    Delete background.js

--- background.js
+++
@@ -1,8 +0,0 @@
- background.js (background script)
-chrome.runtime.onInstalled.addListener(function() {
-   This function runs when the extension is installed or updated.
-});
-
-chrome.runtime.onStartup.addListener(function() {
-   This function runs when Chrome starts up.
-});
